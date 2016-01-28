26d25f894597061acb4a9368d449b40ffd4f03e3
wangyaolin <wangyaolin1112@gmail.com>
2016-01-27 23:03:42 -0800

    add only 23

---
+++ src/MergeKSortedLists.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class MergeKSortedLists {
+
+	
+	
+	// min-heap --> PriorityQueue
+	public ListNode mergeKLists(ListNode[] lists) {
+        int num = lists.length;
+        if(num == 0) return null;
+        if(num == 1) return lists[0];
+        
+        ////////////REMEBER!!! new PriorityQueue<Object>(size, comparator);
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.length, new Comparator<ListNode>() {
+																		public int compare(ListNode o1, ListNode o2) {
+																			return o1.val - o2.val;
+																		}
+        															});
+        
+        for(ListNode list : lists) {
+        	if(list != null) pq.add(list);
+        }
+        
+        ListNode head = new ListNode(0);
+        ListNode point = head;
+        
+        while(pq.size() > 0) {
+        	ListNode tmp = pq.poll();
+        	point.next = tmp;
+        	
+        	// keep adding next element of each list
+        	if(tmp.next != null) pq.add(tmp.next);
+        	
+        	point = point.next;
+        }
+        
+        return head.next;
+        
+    }
+}
