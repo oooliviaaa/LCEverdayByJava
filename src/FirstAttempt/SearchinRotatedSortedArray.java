51e279d13230632bf698c10cdcdf40a0e338ccef
wangyaolin <wangyaolin1112@gmail.com>
2016-02-08 17:33:00 -0800

    only 33

---
+++ src/FirstAttempt/SearchinRotatedSortedArray.java
@@ -0,0 +1,58 @@
+package FirstAttempt;
+
+public class SearchinRotatedSortedArray {
+
+	public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) {
+        	return -1;
+        }
+        
+        int bg = 0;
+        int ed = nums.length-1;
+        
+        while(bg+1 < ed) {  ///////// make sure bg never exceed ed, so that make sure ed never less than 0
+										////////////////// and bg never exceed nums.length-1
+        	System.out.println(bg + " ===== " + ed);
+        	int mid = (bg + ed) / 2;
+        	if(nums[mid] == target) return mid;
+        	if(nums[mid] > nums[bg]) {  // mid is in the front of cutting point
+        		if(nums[mid] >= target & target >= nums[bg]) {
+        			ed = mid-1; 
+        		}
+        		else{
+        			bg = mid+1;
+        		}
+        	}
+        	else {
+        		if(nums[mid] <= target && target <= nums[ed]) {
+        			bg = mid+1;
+        		}
+        		else {
+        			ed = mid-1;
+        		}
+        	}
+        }
+        if(nums[bg] == target) return bg;  ////////////in the end, when bg and ed are near by each other, check both
+        if(nums[ed] == target) return ed;
+        return -1;
+    }
+	
+	
+	public static void main(String[] args) {
+		int[] nums = new int[]{5,6,7,8,9,0,1,2,3,4};
+		SearchinRotatedSortedArray sr = new SearchinRotatedSortedArray();
+		int res0 = sr.search(nums, 0);
+		int res1 = sr.search(nums, 2);
+		System.out.println("res0 >>>" + res0);
+		System.out.println("res1 >>>" + res1);
+
+		int res2 = sr.search(nums, 9);
+		int res3 = sr.search(nums, 7);
+		System.out.println("res2 >>>" + res2);
+		System.out.println("res3 >>>" + res3);
+
+		int res4 = sr.search(nums, 23);
+		System.out.println("res4 >>>" + res4);
+		
+	}
+}
