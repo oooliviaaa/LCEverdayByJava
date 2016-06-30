4692271e5d4a68651d313fd68ed91a09da4bb29c
wangyaolin <wangyaolin1112@gmail.com>
2016-06-30 00:02:34 -0700

    add odd even linkedlist

---
+++ src/FirstAttempt/OddEvenLinkedList.java
@@ -0,0 +1,27 @@
+package FirstAttempt;
+
+public class OddEvenLinkedList {
+
+	// http://www.programcreek.com/2015/03/leetcode-odd-even-linked-list-java/
+	public ListNode oddEvenList(ListNode head) {
+        if (head == null) return head;
+        
+        ListNode p = head;
+        ListNode q = head.next;
+        ListNode connectNode = head.next;
+        
+        while (p != null && q != null) {
+        	if (q.next == null) {
+        		break;    ///////////// 如果有偶数个，就要在p，q变成null之前跳出
+        	}
+        	
+        	ListNode pNext = p.next;
+        	p.next = q.next;
+        	p = p.next;
+        	q = p.next;
+        	
+        }
+        p.next = connectNode;
+        return head;
+    }
+}
