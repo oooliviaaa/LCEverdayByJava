6137124ccfe7e9667294c82fe81729e407ac4316
yaolin <yaolin@yahoo-inc.com>
2016-08-23 22:50:47 -0700

    add clone graph

--- src/FirstAttempt/CloneGraph.java
+++ src/FirstAttempt/CloneGraph.java
@@ -1,7 +1,9 @@
 package FirstAttempt;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CloneGraph {
 
@@ -15,7 +17,31 @@ public class CloneGraph {
 	};
 	
 	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) return null;
         
+        Map<UndirectedGraphNode, UndirectedGraphNode> note = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+        
+        UndirectedGraphNode head = new UndirectedGraphNode(node.label);
+        
+        note.put(node, head);
+        dfs(node, note);
+        return head;
     }
+	
+	private void dfs(UndirectedGraphNode node, Map<UndirectedGraphNode, UndirectedGraphNode> note) {
+		if (node == null) return;
+		
+		List<UndirectedGraphNode> neighbors = node.neighbors;
+		for (UndirectedGraphNode n : neighbors) {
+			if (!note.containsKey(n)) {
+				UndirectedGraphNode newN = new UndirectedGraphNode(n.label);
+				note.put(n, newN);
+				dfs(n, note);
+			}
+			UndirectedGraphNode newNode = note.get(node);
+			UndirectedGraphNode newN = note.get(n);
+			newNode.neighbors.add(newN);   //////////// get connected !!!!!
+		}
+	}
 	
 }
