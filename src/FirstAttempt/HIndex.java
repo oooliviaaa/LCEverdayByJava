39830191269beaf14984890a78639352bf0c61a5
yaolin <yaolin@yahoo-inc.com>
2016-10-03 16:11:39 -0700

    sync

---
+++ src/FirstAttempt/HIndex.java
@@ -0,0 +1,9 @@
+package FirstAttempt;
+
+public class HIndex {
+
+	// https://segmentfault.com/a/1190000003794831
+	public int hIndex(int[] citations) {
+        
+    }
+}
---
+++ src/FirstAttempt/HIndexII.java
@@ -0,0 +1,10 @@
+package FirstAttempt;
+
+public class HIndexII {
+
+	
+	// https://segmentfault.com/a/1190000003794831
+	public int hIndex(int[] citations) {
+        
+    }
+}
