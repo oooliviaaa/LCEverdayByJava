3477ca5a7076aa2b96a55ffd6d75ff4bacfb87ff
yaolin <yaolin@yahoo-inc.com>
2016-10-03 15:16:09 -0700

    add digits

---
+++ src/FirstAttempt/AddDigits.java
@@ -0,0 +1,18 @@
+package FirstAttempt;
+
+public class AddDigits {
+
+	
+	public int addDigits(int num) {
+        
+		while (num > 9) {
+			int sum = 0;
+			while(num > 0) {
+				sum += num%10;
+				num /= 10;
+			}
+			num = sum;
+		}
+		return num;
+    }
+}
--- src/FirstAttempt/MeetingRoomsII.java
+++ src/FirstAttempt/MeetingRoomsII.java
@@ -11,10 +11,12 @@ public class MeetingRoomsII {
 	/**
 	 * 用贪心法，即从第一个时间段开始，选择下一个最近不冲突的时间段，再选择下一个最近不冲突的时间段，直到没有更多。
 	 * 然后如果有剩余时间段，开始为第二个房间安排，选择最早的时间段，再选择下一个最近不冲突的时间段，直到没有更多，如果还有剩余时间段，则开辟第三个房间，以此类推。
+	 * 
 	 * 这里的技巧是我们不一定要遍历这么多遍，我们实际上可以一次遍历的时候就记录下，
 	 * 比如第一个时间段我们放入房间1，然后第二个时间段，如果和房间1的结束时间不冲突，就放入房间1，否则开辟一个房间2。
 	 * 然后第三个时间段，如果和房间1或者房间2的结束时间不冲突，就放入房间1或者2，否则开辟一个房间3，依次类推，最后统计开辟了多少房间。
 	 * 对于每个房间，我们只要记录其结束时间就行了，这里我们查找不冲突房间时，只要找结束时间最早的那个房间。
+	 * 
 	 * 这里还有一个技巧，如果我们把这些房间当作List来管理，每次查询需要O(N)时间，如果我们用堆来管理，可以用logN时间找到时间最早结束的房间。
 	 * */
 	public int minMeetingRooms(Interval[] intervals) {
@@ -30,7 +32,7 @@ public class MeetingRoomsII {
         
         for (int i = 1; i < intervals.length; i++) {
         	Interval cur = intervals[i];
-        	if (cur.start >= endTimes.peek()) {
+        	if (cur.start >= endTimes.peek()) {   // endtime最早的一个房间空闲了。
         		endTimes.poll();
         	}
         	endTimes.offer(cur.end);
