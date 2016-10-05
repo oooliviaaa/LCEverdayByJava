ef8933b5aa7dced4a69b93b1427b3c7d0047c742
yaolin <yaolin@yahoo-inc.com>
2016-10-04 18:04:50 -0700

    add paint house

---
+++ src/FirstAttempt/PaintHouseII.java
@@ -0,0 +1,43 @@
+package FirstAttempt;
+
+public class PaintHouseII {
+
+	// https://segmentfault.com/a/1190000003903965
+	/**
+	 * 和I的思路一样，不过这里我们有K个颜色，不能简单的用Math.min方法了。
+	 * 如果遍历一遍颜色数组就找出除了自身外最小的颜色呢？我们只要把最小和次小的都记录下来就行了，
+	 * 这样如果和最小的是一个颜色，就加上次小的开销，反之，则加上最小的开销。
+	 * */
+	public int minCostII(int[][] costs) {
+		if(costs != null && costs.length == 0) return 0;
+		int len = costs.length;   // house
+		int k = costs[0].length;  // color
+		
+		int preMin = 0;
+		int preSecMin = 0;
+		int preHouse = 0;
+		
+		for (int i = 0; i < len; i++) {
+			
+			int curMin = Integer.MAX_VALUE;
+			int curSecMin = Integer.MAX_VALUE;
+			int curHouse = -1;
+			
+			for (int j = 0; j < k; j++) {
+				costs[i][j] += (j == preHouse ? preSecMin : preMin);
+				if (costs[i][j] <= curMin) {
+					curSecMin = curMin;
+					curMin = costs[i][j];
+					curHouse = j;
+				} else if (costs[i][j] < curSecMin) {
+					curSecMin = costs[i][j];
+				}
+			}
+			
+			preMin = curMin;
+			preSecMin = curSecMin;
+			preHouse = curHouse;
+		}
+		return preMin;
+    }
+}
