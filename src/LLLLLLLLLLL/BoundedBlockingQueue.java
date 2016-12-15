ceb51690cb303a3b48828fa963b305b6acf7d2b8
yaolin <yaolin@yahoo-inc.com>
2016-12-14 18:46:18 -0800

    sync

--- src/LLLLLLLLLLL/BoundedBlockingQueue.java
+++ src/LLLLLLLLLLL/BoundedBlockingQueue.java
@@ -1,25 +1,76 @@
 package LLLLLLLLLLL;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class BoundedBlockingQueue {
+	
+	int cap;
+	Queue<Object> queue;
+	Lock readLock;
+	Lock writeLock;
+	Condition emptyCond;
+	Condition fullCond;
+	
 	public void init(int capacity) throws Exception {
-		
+		this.cap = capacity;
+		queue = new LinkedList<Object>();
+		ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+		readLock = readWriteLock.readLock();
+		writeLock = readWriteLock.writeLock();
+		emptyCond = readLock.newCondition();
+		fullCond = writeLock.newCondition();
 	}
 	 
 	  
-	  public Object get() throws Exception {
-		  
-	  }
+	public Object get() throws Exception {
+		readLock.lock();
+		try {
+			while (queue.size() == 0) {
+				emptyCond.wait();
+			}
+			Object obj = queue.poll();
+			fullCond.signal();
+			return obj;
+		} finally {
+			readLock.unlock();
+		}
+	}
 	 
 	  
-	  public void put(Object obj) throws Exception {
-		  
-	  }
-	 
-	  
-	  public void multiput(List<Object> objs) throws Exception {
-		  
-	  }
+	public void put(Object obj) throws Exception {
+		writeLock.lock();
+		readLock.lock();
+		try {
+			while (queue.size() == cap) {
+				fullCond.wait();
+			}
+			queue.offer(obj);
+			emptyCond.signal();
+		} finally {
+			readLock.unlock();
+			writeLock.unlock();
+		}
+	}
+
+
+	public void multiput(List<Object> objs) throws Exception {
+		writeLock.lock();
+		readLock.lock();
+		try {
+			for (Object obj : objs) {
+				put(obj);
+			}
+		} finally {
+			readLock.unlock();
+			writeLock.unlock();
+		}
+	}
 	  
 }
--- src/LLLLLLLLLLL/MinDiffOfTwoArrays.java
+++ src/LLLLLLLLLLL/MinDiffOfTwoArrays.java
@@ -4,6 +4,31 @@ public class MinDiffOfTwoArrays {
 
 	
 	public int getMinDiff(int[] a, int[] b) {
+		int[][] dp = new int[a.length][b.length];
 		
+		dp[0][0] = Math.abs(a[0] - b[0]);
+		for (int i = 1; i < b.length; i++) {
+			dp[0][i] = Math.min(dp[0][i-1], Math.abs(a[0] - b[i]));
+		}
+		
+		for (int i = 1; i < a.length; i++) {
+			dp[i][i] = dp[i-1][i-1] + Math.abs((a[i] - b[i]));
+		}
+		
+		for (int i = 1; i < a.length; i++) {
+			for (int j = i+1; j < b.length; j++) {
+				dp[i][j] = Math.min(dp[i-1][j-1] + Math.abs(a[i] - b[j]), dp[i][j-1]);
+			}
+		}
+		return dp[a.length-1][b.length-1];
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {1,3,12};
+		int[] b = {4,9,8,4};
+		
+		MinDiffOfTwoArrays md = new MinDiffOfTwoArrays();
+		int res = md.getMinDiff(a, b);
+		System.out.println(res);
 	}
 }
